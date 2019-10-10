861d15b5f008f72639b224543c5348a04387c163
Steffany Bahamon <sbahamon1@gmail.com>
2019-10-09 23:21:06 -0500

    setting up app

---
+++ bot.py
@@ -0,0 +1,17 @@
+import requests
+import tweepy
+from os import environ
+
+CONSUMER_KEY = environ['CONSUMER_KEY']
+CONSUMER_SECRET = environ['CONSUMER_SECRET']
+ACCESS_KEY = environ['ACCESS_KEY']
+ACCESS_SECRET = environ['ACCESS_SECRET']
+# Authenticate to Twitter
+auth = tweepy.OAuthHandler("CONSUMER_KEY", "CONSUMER_SECRET")
+auth.set_access_token("ACCESS_TOKEN", "ACCESS_TOKEN_SECRET")
+
+# Create API object
+api = tweepy.API(auth)
+
+# Create a tweet
+api.update_status("Hello Tweepy")
