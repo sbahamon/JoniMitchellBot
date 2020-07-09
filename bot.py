6da4cb473530657f8ed031441418d1f7ffca5d23
sbahamon <sbahamon1@gmail.com>
2020-07-08 20:11:03 -0500

    added heroku auth

--- bot.py
+++ bot.py
@@ -5,10 +5,19 @@ import credentials as cd
 
 from genius import *
 
+
+# local authentication
 consumerKey = credentials.consumerKey
 consumerSecret = credentials.consumerSecret
 twitterApiKey = credentials.twitterApiKey
 twitterApiSecretKey = credentials.twitterApiSecretKey
+
+# heroku authentication
+# CONSUMER_KEY = environ['CONSUMER_KEY']
+# CONSUMER_SECRET = environ['CONSUMER_SECRET']
+# ACCESS_KEY = environ['ACCESS_KEY']
+# ACCESS_SECRET = environ['ACCESS_SECRET']
+
 
 def main():
     # Example searches
@@ -24,9 +33,13 @@ def main():
     tweet = get_lyrics_tweet(retrieve_lyrics(songs[lucky_song]))
     print(tweet)
 
-
+    # local authentication
     auth = tweepy.OAuthHandler(consumerKey, consumerSecret)
     auth.set_access_token(twitterApiKey, twitterApiSecretKey)
+
+    # heroku authentication
+    # auth = tweepy.OAuthHandler(cd.CONSUMER_KEY, cd.CONSUMER_SECRET)
+    # auth.set_access_token(cd.ACCESS_KEY, cd.ACCESS_SECRET)
 
     api = tweepy.API(auth)
     api.update_status(tweet)
